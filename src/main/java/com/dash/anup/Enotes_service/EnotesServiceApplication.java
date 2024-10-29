f1566d37e3791505968810a946f3c623d09f80a8
Anup Dash <dashanup4@gmail.com>
2024-10-29 15:44:20 +0530

    initial commit

---
+++ src/main/java/com/dash/anup/Enotes_service/EnotesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dash.anup.Enotes_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dash/anup/Enotes_service/EnotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dash.anup.Enotes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
